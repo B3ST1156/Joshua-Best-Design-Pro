106883b1eb68890ace93767d47d628cf445c08c5
B3ST1156 <Scotty1156@gmail.com>
2017-11-21 13:41:05 -0800

    This will be my new website

---
+++ js/Plugin.js
@@ -0,0 +1,4 @@
+// Jquery plugin 
+
+// KeyFrame Plugin------------------------------->
+
---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+// My awesome scripts!!!
+
+console.log('Whatsup!!!');
+
+$('video').mediaelementplayer();
+
+$('.hamburger-menu').on('click', function() {
+$('.bar').toggleClass('animate');
+$('.hand').toggleClass('active');
+$('nav').toggleClass('activenav');
+
+
+    
+//$('document').querySelector(".hamburguer").addEventListener("click", function(){
+//$('document').querySelector(".full-menu").classList.toggle("active");
+
+
+
+
+});
+
+
+
+
+        
+    
