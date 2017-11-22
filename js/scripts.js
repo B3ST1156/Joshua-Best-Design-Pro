f91441f49c518cc97b53db4f4048db531e2ca202
B3ST1156 <Scotty1156@gmail.com>
2017-11-21 18:43:59 -0800

    Encountered a problem
    my .json file is writing to the wrong folder and as such the changes I
    make to the main.scss style sheet are not overwriting the previous
    style sheet.

--- js/scripts.js
+++ js/scripts.js
@@ -8,28 +8,9 @@ $('.hamburger-menu').on('click', function() {
 $('.bar').toggleClass('animate');
 $('.hand').toggleClass('active');
 $('nav').toggleClass('activenav');
-    
-// Randomize Header
 
-// Store & Generate Number
-var maxRand = 3;
-var numRand = Math.floor( Math.random() * maxRand );
-//console.log(numRand);
+// This is for Lightbox----------------------------------------->
 
-// Store images
-var img0 = 'http://picsum.photos/200/300';
-var img1 = 'http://picsum.photos/201/301';
-var img2 = 'http://picsum.photos/202/302';
-
-// Show Images
-$('header img').attr('src', eval('img' + numRand) );
-
-// Activate Gallery
-$('figure').lightGallery({
-    selector: 'a', 
-    thumbnail: true,
-    share: false
-});
 
 
     
