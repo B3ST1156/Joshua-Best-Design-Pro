0249cafc4980f0fd49487fb497bcbe03d0f3fbdb
B3ST1156 <Scotty1156@gmail.com>
2017-12-08 18:27:15 -0800

    at a stopping point for finals

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,24 @@
 
 console.log('Whatsup!!!');
 
+
+
 $('video').mediaelementplayer();
+
+// Activate Gallery
+$('figure').lightGallery({
+    selector: 'a', 
+    thumbnail: true,
+    share: false,
+    download: false,
+    mode: 'lg-fade',
+    cssEasing : 'cubic-bezier(0.25, 0, 0.25, 1)'});
 
 $('.hamburger-menu').on('click', function() {
 $('.bar').toggleClass('animate');
 $('.hand').toggleClass('active');
 $('nav').toggleClass('activenav');
-
+  
 // This is for Lightbox----------------------------------------->
 
 
@@ -16,11 +27,17 @@ $('nav').toggleClass('activenav');
     
 //$('document').querySelector(".hamburguer").addEventListener("click", function(){
 //$('document').querySelector(".full-menu").classList.toggle("active");
+});
 
-
+$('#P').on('click', function(){
+$('main').toggleClass('mainActive');
 
 
 });
+
+
+
+
 
 
 
